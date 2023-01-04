7f490f0d036ccf0b276b9e26ca0fe016d6b90481
oliverowen2210 <oliverowen2210@gmail.com>
2023-01-04 04:01:37 -0600

    deleted template routers
    deleted template routes

--- app.js
+++ app.js
@@ -4,8 +4,7 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
+const inventoryRouter = require("./routes/inventory");
 
 var app = express();
 
@@ -27,8 +26,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
+app.use("/inventory", inventoryRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
