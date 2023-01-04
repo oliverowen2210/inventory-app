d067b46001364bf3812d743e43b10bc0f6898df1
oliverowen2210 <oliverowen2210@gmail.com>
2023-01-04 04:15:33 -0600

    made "/" route use inventory router

--- app.js
+++ app.js
@@ -26,6 +26,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
+app.use("/", inventoryRouter);
 app.use("/inventory", inventoryRouter);
 
 // catch 404 and forward to error handler
