4f6061c87fc3838ff74efbbd5d84e8684ef0f5fb
oliverowen2210 <oliverowen2210@gmail.com>
2023-01-04 18:24:23 -0600

    index route gets categories from DB

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -7,6 +7,9 @@ const async = require("async");
 exports.index = (req, res) => {
   async.parallel(
     {
+      categories(cb) {
+        Category.find({}).exec(cb);
+      },
       category_count(cb) {
         Category.countDocuments({}, cb);
       },
@@ -16,9 +19,15 @@ exports.index = (req, res) => {
     },
     function (err, results) {
       if (err) return next(err);
+      if (results.categories === null) {
+        err = new Error("No categories have been created.");
+        err.status = 404;
+        return next(err);
+      }
 
       res.render("index", {
         title: "Inventory App",
+        categories: results.categories,
         category_count: results.category_count,
         item_count: results.item_count,
       });
