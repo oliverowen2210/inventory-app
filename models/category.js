0b0c1149fe9b5b4bddccb71fb8a62376c44e7d72
oliverowen2210 <oliverowen2210@gmail.com>
2023-01-04 18:24:37 -0600

    removed redundant "items" field

--- models/category.js
+++ models/category.js
@@ -4,7 +4,6 @@ const Schema = mongoose.Schema;
 const CategorySchema = new Schema({
   name: { type: String, required: true },
   description: { type: String, required: true },
-  items: [{ type: Schema.Types.ObjectId, ref: "Item" }],
 });
 
 CategorySchema.virtual("URL").get(function () {
