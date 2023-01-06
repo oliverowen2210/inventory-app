70fa03a78d74dde4bff9788ae1d8f4296fcd680c
oliverowen2210 <oliverowen2210@gmail.com>
2023-01-06 09:06:37 -0600

    updated router to use new creating/deleting routes

--- routes/inventory.js
+++ routes/inventory.js
@@ -11,4 +11,23 @@ router.get(
   categoryController.category_create_get
 );
 
+router.post(
+  "/inventory/category/create",
+  categoryController.category_create_post
+);
+
+router.get("/inventory/category/:id", categoryController.category_detail);
+
+router.get(
+  "/inventory/category/:id/delete",
+  categoryController.category_delete_get
+);
+router.post(
+  "/inventory/category/:id/delete",
+  categoryController.category_delete_post
+);
+
+router.get("/inventory/item/create", itemController.item_create_get);
+router.post("/inventory/item/create", itemController.item_create_post);
+
 module.exports = router;
