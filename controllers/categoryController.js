88cadb9a1edb270e1e90f26eb4ac125a7b6ef75f
oliverowen2210 <oliverowen2210@gmail.com>
2023-01-06 09:07:52 -0600

    renamed index to category_list in controller

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -25,7 +25,7 @@ exports.categories = (req, res) => {
         return next(err);
       }
 
-      res.render("index", {
+      res.render("category_list", {
         title: "Inventory App",
         categories: results.categories,
         category_count: results.category_count,
@@ -39,4 +39,101 @@ exports.category_create_get = (req, res) => {
   res.render("category_form", { title: "New category" });
 };
 
-exports.category_create_post = (req, res) => {};
+exports.category_create_post = [
+  body("name", "A category name is required.")
+    .isLength({ min: 1, max: 110 })
+    .trim()
+    .escape(),
+  body("description", "A category description is required.")
+    .isLength({ min: 1, max: 20 })
+    .trim()
+    .escape(),
+
+  (req, res, next) => {
+    const errors = validationResult(req);
+
+    const category = new Category({
+      name: req.body.name,
+      description: req.body.description,
+    });
+
+    if (!errors.isEmpty()) {
+      res.render("category_form", { title: "New category", category, errors });
+    } else {
+      category.save((err, new_category) => {
+        if (err) return next(err);
+        res.redirect(new_category.URL);
+      });
+    }
+  },
+];
+
+exports.category_detail = (req, res, next) => {
+  async.parallel(
+    {
+      category(cb) {
+        Category.findById(req.params.id).exec(cb);
+      },
+      items(cb) {
+        Item.find({ category: req.params.id }).exec(cb);
+      },
+    },
+    function (err, results) {
+      if (err) return next(err);
+      if (results.category === null) {
+        err = new Error("Category not found");
+        err.status = 404;
+        return next(err);
+      }
+
+      res.render("category_detail", {
+        title: "Category Detail",
+        category: results.category,
+        items: results.items,
+      });
+    }
+  );
+};
+
+exports.category_delete_get = (req, res, next) => {
+  async.parallel(
+    {
+      category(callback) {
+        Category.findById(req.params.id).exec(callback);
+      },
+      category_items(callback) {
+        Item.find({ category: req.params.id }).exec(callback);
+      },
+    },
+    (err, results) => {
+      if (err) return next(err);
+      if (results.category === null) {
+        err = new Error("No category with that ID was found.");
+        err.status = 404;
+        return next(err);
+      }
+      res.render("category_delete", {
+        title: "Delete category",
+        category: results.category,
+        category_items: results.category_items,
+      });
+    }
+  );
+};
+
+exports.category_delete_post = (req, res, next) => {
+  async.parallel(
+    {
+      remove_items(callback) {
+        Item.remove({ category: req.body.id }).exec(callback);
+      },
+      remove_category(callback) {
+        Category.findByIdAndRemove(req.body.id).exec(callback);
+      },
+    },
+    (err, results) => {
+      if (err) return next(err);
+      res.redirect("/");
+    }
+  );
+};
