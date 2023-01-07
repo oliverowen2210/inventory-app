5a1c80109285011e96c5d61bc6673ab09d367dbf
oliverowen2210 <oliverowen2210@gmail.com>
2023-01-07 08:22:22 -0600

    added new route functions and commented sections

--- routes/inventory.js
+++ routes/inventory.js
@@ -4,19 +4,18 @@ const router = express.Router();
 const categoryController = require("../controllers/categoryController");
 const itemController = require("../controllers/itemController");
 
+/**Home page */
 router.get("/", categoryController.categories);
 
+/** Category Routes */
 router.get(
   "/inventory/category/create",
   categoryController.category_create_get
 );
-
 router.post(
   "/inventory/category/create",
   categoryController.category_create_post
 );
-
-router.get("/inventory/category/:id", categoryController.category_detail);
 
 router.get(
   "/inventory/category/:id/delete",
@@ -27,7 +26,26 @@ router.post(
   categoryController.category_delete_post
 );
 
+router.get(
+  "/inventory/category/:id/update",
+  categoryController.category_update_get
+);
+router.post(
+  "/inventory/category/:id/update",
+  categoryController.category_update_post
+);
+
+/**Category detail/item list*/
+router.get("/inventory/category/:id", categoryController.category_detail);
+
+/**Item routes */
 router.get("/inventory/item/create", itemController.item_create_get);
 router.post("/inventory/item/create", itemController.item_create_post);
+
+router.get("/inventory/item/:id/delete", itemController.item_delete_get);
+router.post("/inventory/item/:id/delete", itemController.item_delete_post);
+
+router.get("/inventory/item/:id/update", itemController.item_update_get);
+router.post("/inventory/item/:id/update", itemController.item_update_post);
 
 module.exports = router;
