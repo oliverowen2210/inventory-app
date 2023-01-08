d969b1263aa3686498b942146d1f87cc7aa61987
oliverowen2210 <oliverowen2210@gmail.com>
2023-01-07 23:26:30 -0600

    re-added deployment changes

--- app.js
+++ app.js
@@ -4,13 +4,16 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
+const helmet = require("helmet");
+
 const inventoryRouter = require("./routes/inventory");
 
 var app = express();
 
 const mongoose = require("mongoose");
-const mongoDB =
+const dev_db_url =
   "mongodb+srv://oliverowen2210:g2yGmzois8d92NH6@cluster0.20vsymq.mongodb.net/inventory?retryWrites=true&w=majority";
+const mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const db = mongoose.connection;
@@ -20,6 +23,7 @@ db.on("error", console.error.bind(console, "wtf"));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 
+app.use(helmet());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
